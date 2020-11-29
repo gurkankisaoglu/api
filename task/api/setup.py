88afa80d29a09618e0df3880cb8c91aca7ccbb55
gurkankisaoglu <gurkan.kisaoglu@plentific.com>
2020-11-29 14:08:36 +0300

    setup py

--- task/api/setup.py
+++ task/api/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="task_service",
+    name="api",
     author="gurkankisaoglu",
     author_email="kisaoglu.gurkan@gmail.com",
     url="https://github.com/gurkankisaoglu/api",
     install_requires=[
         # Packages from PyPi
     ],
-    packages=["task_service"],
+    packages=["api"],
     include_package_data=True,
     zip_safe=False,
 )
